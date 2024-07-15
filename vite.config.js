6f02e7fe187eb6db95a7ae6753c5b18bf486d323
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-15 18:02:05 +0200

    correzione file env

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import 'resurces/scss/app.scss';
 const path = require('path');
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                // Modifichiamo il percorso del css usando sass
-                'resources/scss/app.scss',
+                'resources/sass/app.scss',
                 'resources/js/app.js'
             ],
             refresh: true,
         }),
     ],
-    //Aggiungiamo un alias per la cartella /resources/
     resolve: {
         alias: {
             '~resources': '/resources/',
